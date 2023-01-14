ebf734f60fbfb47271c1a90475a6ee63c4c3ad26
dev-yun <yunpro2766@gmail.com>
2023-01-14 23:46:20 +0900

    Feat: 게시글 작성하기 버튼 완성 - UI 추가, 클릭 시 navigate 기능 추가 #14

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
+import Button from "../components/Button";
 import Header from "../components/Header/index";
 import Component from "../core/Component";
+import { navigateTo } from "../router";
+
+import "../styles/home.scss";
 
 class Home extends Component {
   template() {
     return `
     <header class='header' style="justify-content:flex-end"></header>
     <main>
-      <div>홈페이지</div>
+      <div class='upload_post_button_container'></div>
       <ul>글 목록</ul>
     </main>
     `;
@@ -14,8 +18,19 @@ class Home extends Component {
 
   mounted() {
     const $header = document.querySelector(".header");
+    const $uploadButtonContainer = document.querySelector(
+      ".upload_post_button_container"
+    );
 
     new Header($header);
+    new Button($uploadButtonContainer, {
+      content: `게시글 작성하기`,
+      onClick: this.goUploadPage,
+    });
+  }
+
+  goUploadPage() {
+    navigateTo("/upload");
   }
 }
 
