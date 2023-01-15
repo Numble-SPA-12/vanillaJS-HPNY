8a3e41d1f1fc645737bbd7ac4d2e055099471a2b
dev-yun <yunpro2766@gmail.com>
2023-01-15 15:57:39 +0900

    Feat: Post 페이지 이미지, content 코드 작성 & UI 구현 #16

---
+++ src/components/PostDetail/index.js
@@ -0,0 +1,33 @@
+import Component from "../../core/Component";
+
+class postDetail extends Component {
+  template() {
+    const { image, title, createdAt, content } = this.$props.post;
+    const convertedTime = this.getStringDate(new Date(createdAt));
+
+    return `
+      <img src=${image} alt="게시글 이미지"/>
+      <strong class="post_detail_title">${title}</strong>
+      <time datetime=${createdAt}>${convertedTime}</time>
+      <p class="post_detail_content">${content}</p>
+    `;
+  }
+
+  getStringDate(date) {
+    const year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+
+    if (month < 10) {
+      month = `0${month}`;
+    }
+
+    if (day < 10) {
+      day = `0${day}`;
+    }
+
+    return `${year}. ${month}. ${day}`;
+  }
+}
+
+export default postDetail;
--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,21 +1,40 @@
+import { getPostDetail } from "../apis/post";
 import Header from "../components/Header";
+import postDetail from "../components/PostDetail";
 import Component from "../core/Component";
+
+import "../styles/post.scss";
 
 class Post extends Component {
   template() {
     return `
     <header class='header'></header>
     <main>
-      <div>상세글 페이지</div>
-      <ul>글 내용</ul>
+      <section class='post_detail_container'>상세글 페이지</section>
     </main>
     `;
   }
 
+  setup() {
+    this.$params = window.location.pathname.split("/")[2];
+    this.$state = {};
+    this.$getPostDetail(this.$params);
+  }
+
+  async $getPostDetail(postId) {
+    const { data } = await getPostDetail(postId);
+    console.log(data);
+    this.setState({ post: data.post, comments: data.comments });
+  }
+
   mounted() {
     const $header = document.querySelector(".header");
+    const $postDetail = document.querySelector(".post_detail_container");
 
     new Header($header);
+    new postDetail($postDetail, {
+      post: this.$state.post,
+    });
   }
 }
 
